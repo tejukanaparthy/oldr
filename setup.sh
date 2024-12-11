7efd25e48569db922b69f7a0439d7d182e1c29bd
Camden Chin <ctc52@duke.edu>
2024-12-11 12:55:17 -0500

    Accessibility features

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing project dependencies..."
-npm install
-
-echo "Initializing the SQLite database..."
-node scripts/initDatabase.js
-
-echo "Setup complete. You can now run the application using 'node app.js'."
